9c4532f65cb4cfc8c098e8fbc89bee5a80ee5e96
VUE Дмитро Гулак <stajectgamer@gmail.com>
2024-02-17 23:22:52 +0200

    master: testing localization

--- src/main.js
+++ src/main.js
@@ -1,38 +1,26 @@
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import { createI18n } from 'vue-i18n';
-import App from './templates/app/App.vue';
-import router from './plugins/router';
-import en from './plugins/locales/en.js';
-import ua from './plugins/locales/ua.js';
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
+import App from './templates/app/App.vue'
+import router from './plugins/router'
+import en from './plugins/locales/en.js'
+import ua from './plugins/locales/ua.js'
 
-const app = createApp(App);
+const app = createApp(App)
 
-app.use(createPinia());
-app.use(router);
+app.use(createPinia())
+app.use(router)
 
 const i18n = createI18n({
-    lazy: true,
-    legacy: false,
-    strategy: "prefix_except_default",
-    locales: [
-        {
-            code: "en",
-            iso: "en-US",
-            file: en,
-            text: "EN",
-        },
-        {
-            code: "ua",
-            iso: "ua-UA",
-            file: ua,
-            text: "UA",
-        },
-    ],
-    defaultLocale: "en",
-    langDir: "locales/",
-});
+  legacy: false,
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages: {
+    en,
+    ua,
+  },
+})
 
-app.use(i18n);
+app.use(i18n)
 
-app.mount('#app');
+app.mount('#app')
--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -1,11 +1,17 @@
 <script setup>
 import { useI18n } from 'vue-i18n';
 
-const { t } = useI18n();
+const { t, locale, availableLocales } = useI18n();
+
+const changeLanguage = (newLocale) => {
+    locale.value = newLocale;
+};
 </script>
 
 <template>
 <h2>{{t('welcome')}}</h2>
+  <button @click="changeLanguage('en')">English</button>
+  <button @click="changeLanguage('ua')">Українська</button>
 </template>
 
 <style scoped>
